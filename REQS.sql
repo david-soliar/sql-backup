18f1e852e1fe10574a72227ec31daa8ad93656ce
David Soliar <dav.soliar@gmail.com>
2024-03-28 00:18:03 +0100

    Add files via upload

---
+++ REQS.sql
@@ -0,0 +1,9 @@
+SELECT * FROM Knihy;
+
+--SELECT DISTINCT zanr FROM Knihy;
+
+--SELECT * FROM Knihy WHERE nazev LIKE "S%";
+
+--SELECT * FROM Knihy WHERE pocet_stran>200;
+
+--SELECT * FROM Knihy WHERE cena BETWEEN 150 AND 250;
